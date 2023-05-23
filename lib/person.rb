5593e3d0f4c442c72a220ac9234a195cfdf89a35
Mike <mike.munene@student.moringaschool.com>
2023-05-23 07:38:08 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
 
+    # setter method
+    def name=(name)
+        @name = name
+    end
+
+    # getter method
+    def name
+        @name
+    end
+
+    # job setter method
+    def job=(job)
+        @job = job
+    end
+
+    # job getter method
+    def job
+        @job
+    end
+
 end
